28e6641da0a3dbf08c07513b397a077bc52fb600
laxmikanthmucharla <laxmikanthmucharla@gmail.com>
2025-11-22 02:02:13 +0530

    Update eks.tf

--- eks-cluster/eks.tf
+++ eks-cluster/eks.tf
@@ -23,7 +23,7 @@ module "eks" {
       min_size       = 1
       max_size       = 3
       desired_size   = 2
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.micro"]
     }
   }
 }
